ea7998c138d1bacbc864b82f74b83cf382d498ed
charlie-sans <gamerboy030607@gmail.com>
2025-07-25 18:20:31 +1000

    Add dependencies and improve error handling in templates
    - Added "uhigh-stdlib" package reference to ClassLibraryTemplate and ConsoleAppTemplate.
    - Updated validation and formatting functions to remove unnecessary braces.
    - Refactored project file to include new resource files for error handling.
    - Created new resource files for error messages with detailed descriptions for compiler and parser errors.
    - Adjusted resource management for better localization support.

--- Diagnostics/Resources/errors.Designer.cs
+++ Diagnostics/Resources/errors.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace uhigh.Properties {
+namespace uhigh.Diagnostics.Resources {
     using System;
     
     
@@ -22,14 +22,14 @@ namespace uhigh.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Resources {
+    public class errors {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources() {
+        internal errors() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace uhigh.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("uhigh.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("uhigh.Diagnostics.Resources.errors", typeof(errors).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
